4673d274c797f4dbc98b0d76a0107e404987881a
Onyekachi Matthew <onyekachimatthew00@gmail.com>
2025-09-05 01:28:45 +0100

    blog

--- blog/views.py
+++ blog/views.py
@@ -5,8 +5,7 @@ from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
-from .serializers import PostSerializer, PostResponseSerializer, PostGetByTitleSerializer, PostGetByidSerializer, \
-    PostUpdateSerializer
+from .serializers import PostSerializer, PostResponseSerializer, PostGetByTitleSerializer, PostGetByidSerializer, PostUpdateSerializer
 from .models import Post
 
 # Create your views here.
